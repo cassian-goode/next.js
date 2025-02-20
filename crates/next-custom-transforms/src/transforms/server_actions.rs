350d168b942bfabf61a039b24ee3946229deaa32
Tim Neutkens <tim@timneutkens.nl>
2025-02-20 15:24:04 +0100

    Ensure pure comment is added

--- crates/next-custom-transforms/src/transforms/server_actions.rs
+++ crates/next-custom-transforms/src/transforms/server_actions.rs
@@ -1911,7 +1911,7 @@ impl<C: Comments> VisitMut for ServerActions<C> {
                                                 {
                                                     ident.span
                                                 } else {
-                                                    PURE_SP
+                                                    DUMMY_SP
                                                 },
                                             )
                                             .into(),
